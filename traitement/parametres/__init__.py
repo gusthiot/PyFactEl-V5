8f6e847009728994928669744974ba7f049240b4
gusthiot <cgusthiot@gmail.com>
2018-06-23 10:35:58 +0200

    - erreur in init

--- traitement/parametres/__init__.py
+++
@@ -1,6 +0,0 @@
-from .edition import Edition
-from .generaux import Generaux
-from .suppression import Suppression
-from .annulation import Annulation
-from .docpdf import DocPdf
-from .paramannexe import Paramannexe
